fded21c484243e7c81fb0559027d6d7363cd18bb
linzhu <lin.lyn.zhu@gmail.com>
2022-03-16 15:28:29 -0700

    check vertically if the word exist using map

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,17 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (let l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+  let arr = [];
+  let length = letters[0].length;
+  for (let i = 0; i <= length - 1; i++) {
+    letters.map(ls => arr.push(ls.shift()));
+    if (arr.join('').includes(word)) return true;
+    arr = [];
+  }
+    
+  return false;
+};
 
-module.exports = wordSearch
+module.exports = wordSearch;
